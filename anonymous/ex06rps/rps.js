6078a9e56767e4fa6275151cc3a3464190a888be
leviseo <seojuhee4@naver.com>
2019-10-15 16:55:04 +0900

    update study ex02

--- anonymous/ex06rps/rps.js
+++ anonymous/ex06rps/rps.js
@@ -1,19 +1,66 @@
-var left = 0;
+var 이미지좌표 = 0;
+var 가위바위보 = { //딕셔너리 자료구조
+	바위: '0',
+	가위: '-142px',
+	보: '-284px',
+};
 
-setInterval(function () {
-	if (left === 0) {
-		left = '-135px';
-	} else if (left === '-135px') {
-		left = '-284px'
-	} else {
-		left = 0;
-	}
-	document.querySelector('#computer').style.background = 
-	'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + left + ' 0';
-}, 10000);
+function 컴퓨터의선택(이미지좌표) {
+	return Object.entries(가위바위보).find(function(v) {
+		return v[1] === 이미지좌표;
+	})[0];
+};
+var 인터벌;
+function 인터벌메이커() {
+	인터벌 = setInterval(function () {
+		if (이미지좌표 === 가위바위보.바위) {
+			이미지좌표 = 가위바위보.가위;
+		} else if (이미지좌표 === 가위바위보.가위) {
+			이미지좌표 = 가위바위보.보
+		} else {
+			이미지좌표 = 가위바위보.바위;
+		}
+		document.querySelector('#computer').style.background = 
+		'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + 이미지좌표 + ' 0';
+	}, 100);	
+};
 
-document.querySelectorAll('.btn').forEach(function (btn) {
-	btn.addEventListener('click', function () {
-		console.log(this.textContent);
+인터벌 = 인터벌메이커();
+
+document.querySelectorAll('.btn').forEach(function(btn) {
+	btn.addEventListener('click', function() {
+		clearInterval(인터벌);
+		setTimeout(function() {
+			인터벌메이커();
+		}, 1000);
+		var 나의선택 = this.textContent;
+		// console.log(나의선택, 컴퓨터의선택(이미지좌표));
+		//1. 가위 0, 바위 1, 보 -1 로 규칙을 찾아서 코드로...
+		//2. 이긴 횟수, 진 횟수, 비긴 횟수 html로 뿌려보자
+		if (나의선택 === '가위') {
+			if (컴퓨터의선택(이미지좌표) === '가위') {
+				console.log('비겼습니다');
+			} else if (컴퓨터의선택(이미지좌표) === '바위') {
+				console.log('졌습니다ㅜㅜ');
+			} else if (컴퓨터의선택(이미지좌표) === '보') {
+				console.log('이겼습니다><');
+			}
+		} else if (나의선택 === '바위') {
+			if (컴퓨터의선택(이미지좌표) === '가위') {
+				console.log('이겼습니다><');
+			} else if (컴퓨터의선택(이미지좌표) === '바위') {
+				console.log('비겼습니다');
+			} else if (컴퓨터의선택(이미지좌표) === '보') {
+				console.log('졌습니다ㅜㅜ');
+			}
+		} else if (나의선택 === '보') {
+			if (컴퓨터의선택(이미지좌표) === '가위') {
+				console.log('졌습니다ㅜㅜ');
+			} else if (컴퓨터의선택(이미지좌표) === '바위') {
+				console.log('이겼습니다><');
+			} else if (컴퓨터의선택(이미지좌표) === '보') {
+				console.log('비겼습니다');
+			}
+		}
 	});	
 });

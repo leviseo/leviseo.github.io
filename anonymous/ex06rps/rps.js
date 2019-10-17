b98b923ed5877af741ead98cd45ce3fb72d8879f
leviseo <seojuhee4@naver.com>
2019-10-17 16:27:24 +0900

    update ex06rps

--- anonymous/ex06rps/rps.js
+++ anonymous/ex06rps/rps.js
@@ -1,8 +1,14 @@
+var 결과창 = document.body.querySelector('.desc');
 var 이미지좌표 = 0;
 var 가위바위보 = { //딕셔너리 자료구조
 	바위: '0',
 	가위: '-142px',
 	보: '-284px',
+};
+var 점수표 = {
+	가위: 1,
+	바위: 0,
+	보: -1
 };
 
 function 컴퓨터의선택(이미지좌표) {
@@ -25,42 +31,57 @@ function 인터벌메이커() {
 	}, 100);	
 };
 
-인터벌 = 인터벌메이커();
+인터벌메이커();
 
 document.querySelectorAll('.btn').forEach(function(btn) {
 	btn.addEventListener('click', function() {
 		clearInterval(인터벌);
 		setTimeout(function() {
+			결과창.textContent = '';
 			인터벌메이커();
 		}, 1000);
 		var 나의선택 = this.textContent;
+		var 나의점수 = 점수표[나의선택];
+		var 컴퓨터점수 = 점수표[컴퓨터의선택(이미지좌표)];
+		var 점수차 = 나의점수 - 컴퓨터점수;
 		// console.log(나의선택, 컴퓨터의선택(이미지좌표));
 		//1. 가위 0, 바위 1, 보 -1 로 규칙을 찾아서 코드로...
-		//2. 이긴 횟수, 진 횟수, 비긴 횟수 html로 뿌려보자
-		if (나의선택 === '가위') {
-			if (컴퓨터의선택(이미지좌표) === '가위') {
-				console.log('비겼습니다');
-			} else if (컴퓨터의선택(이미지좌표) === '바위') {
-				console.log('졌습니다ㅜㅜ');
-			} else if (컴퓨터의선택(이미지좌표) === '보') {
-				console.log('이겼습니다><');
-			}
-		} else if (나의선택 === '바위') {
-			if (컴퓨터의선택(이미지좌표) === '가위') {
-				console.log('이겼습니다><');
-			} else if (컴퓨터의선택(이미지좌표) === '바위') {
-				console.log('비겼습니다');
-			} else if (컴퓨터의선택(이미지좌표) === '보') {
-				console.log('졌습니다ㅜㅜ');
-			}
-		} else if (나의선택 === '보') {
-			if (컴퓨터의선택(이미지좌표) === '가위') {
-				console.log('졌습니다ㅜㅜ');
-			} else if (컴퓨터의선택(이미지좌표) === '바위') {
-				console.log('이겼습니다><');
-			} else if (컴퓨터의선택(이미지좌표) === '보') {
-				console.log('비겼습니다');
-			}
+		if (점수차 === 0) {
+			console.log('비겼습니다');
+			결과창.textContent = '비겼습니다';
+		} else if ([-1, 2].includes(점수차)) {
+			console.log('이겼습니다');
+			결과창.textContent = '이겼습니다';
+		} else {
+			console.log('졌습니다');
+			결과창.textContent = '졌습니다';
 		}
+		//2. 이긴 횟수, 진 횟수, 비긴 횟수 html로 뿌려보자
+		
+		// if (나의선택 === '가위') {
+		// 	if (컴퓨터의선택(이미지좌표) === '가위') {
+		// 		console.log('비겼습니다');
+		// 	} else if (컴퓨터의선택(이미지좌표) === '바위') {
+		// 		console.log('졌습니다ㅜㅜ');
+		// 	} else if (컴퓨터의선택(이미지좌표) === '보') {
+		// 		console.log('이겼습니다><');
+		// 	}
+		// } else if (나의선택 === '바위') {
+		// 	if (컴퓨터의선택(이미지좌표) === '가위') {
+		// 		console.log('이겼습니다><');
+		// 	} else if (컴퓨터의선택(이미지좌표) === '바위') {
+		// 		console.log('비겼습니다');
+		// 	} else if (컴퓨터의선택(이미지좌표) === '보') {
+		// 		console.log('졌습니다ㅜㅜ');
+		// 	}
+		// } else if (나의선택 === '보') {
+		// 	if (컴퓨터의선택(이미지좌표) === '가위') {
+		// 		console.log('졌습니다ㅜㅜ');
+		// 	} else if (컴퓨터의선택(이미지좌표) === '바위') {
+		// 		console.log('이겼습니다><');
+		// 	} else if (컴퓨터의선택(이미지좌표) === '보') {
+		// 		console.log('비겼습니다');
+		// 	}
+		// }
 	});	
 });

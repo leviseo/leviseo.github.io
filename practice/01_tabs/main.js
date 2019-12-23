a81b7f234705703ffd6c2b1d034c8fcfd3bd3d15
leviseo <seojuhee4@naver.com>
2019-12-23 19:38:22 +0900

    update practice/01_tab 코드리뷰

--- practice/01_tabs/main.js
+++ practice/01_tabs/main.js
@@ -15,6 +15,10 @@
 			btn_tab1~3 중에 on 가진 녀석 찾기
 
 		*/
+		// 1. var let 구분하여 사용하기
+		// 2. 공통사용되는 함수를 엄격히 구분하기
+		// 3. 순수함수라는게 있따. 변화는 파라미터로.. 내부는 순수하게 리턴만을 제공 
+		// 4. tabIndext, tabContentsIndex 변수 (state) increaseIndex(value) decrease (value) => value -1/+1
     var ctx = document.getElementById("container");
     var tabs = ctx.querySelectorAll(".btn_tab");
     var tabContents = ctx.querySelectorAll(".tab_content");
@@ -30,6 +34,7 @@
       el.classList.add(className);
     }
 
+		// 정의가 빠진게 있따. 파라미터로 받아서 핸들링 한다는 가정이 우선이 되어야 한다.
     function removeClass() {
       Array.prototype.forEach.call(tabs, function(el) {
         el.classList.remove("on");
@@ -39,7 +44,8 @@
       });
     }
 
-    for (let i = 0; i < tabs.length; i++) {
+		// removeAll , addAll - (removeAll -> addClass) => removeAllAndAddOneClass
+    for (var i = 0; i < tabs.length; i++) {
       tabs[i].addEventListener("click", function() {
         removeClass();
         addClass(tabs[i], "on");
@@ -55,6 +61,15 @@
 		*/
 
     btnNextEl.addEventListener("click", function() {
+
+			// 1. removeAll(elems, 'on')
+			// 2. isAbleToMoveNext(tabIndex) => true / false
+			// 3. increase(tabIndex, 1) / decrease(tabIndex, 1) => 1은 var MOVE_STEP = 1
+			// 4. addClass(elem, 'on')
+			// 5. addClass(elem, 'visible')
+			// 6. eventHandler로 만든다. onNextBtnClick
+			// 7. btnNextEl.addEventKistener('click', onNextBtnClickHandler)
+
 			removeClass();
 			if (tabIndext >= tabs.length -1) {
 				tabIndext = 0;
@@ -80,6 +95,6 @@
 			addClass(tabContents[tabContentsIndex], 'visible');
 		});
 		
-
+		
   };
 })();

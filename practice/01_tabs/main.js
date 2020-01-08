087953b71db91fcee0ec491bd96ed2a8f6fbb102
leviseo <seojuhee4@naver.com>
2020-01-09 01:00:06 +0900

    update practice 01_tabs refactoring

--- practice/01_tabs/main.js
+++ practice/01_tabs/main.js
@@ -13,24 +13,38 @@
 			next += 1, 배열 마지막이면 0으로
 			
 			btn_tab1~3 중에 on 가진 녀석 찾기
+		
+		* [] btn disabled 처리
+		* [] 무한 < > 녀석 추가
 
 		*/
-		// 1. [] var let 구분하여 사용하기
-		// 2. [] 공통사용되는 함수를 엄격히 구분하기
-		// 3. [] 순수함수라는게 있따. 변화는 파라미터로.. 내부는 순수하게 리턴만을 제공 
-		// 4. [] tabIndext, tabContentsIndex 변수 (state) increaseIndex(value) decrease (value) => value -1/+1
+		// 1. var let 구분하여 사용하기
+		// 2. 공통사용되는 함수를 엄격히 구분하기
+		// 3. 순수함수라는게 있따. 변화는 파라미터로.. 내부는 순수하게 리턴만을 제공 
+		// 4. tabIndext, tabContentsIndex 변수 (state) increaseIndex(value) decrease (value) => value -1/+1
+
+		// const
+		var MOVE_STEP = 1;
+		// value, state
+		var tabIndext = 0;
+		var tabContentsIndex = 0;
+		//element
     var ctx = document.getElementById("container");
     var tabs = ctx.querySelectorAll(".btn_tab");
     var tabContents = ctx.querySelectorAll(".tab_content");
-
     var btnPrevEl = ctx.querySelector(".pagenation > .btn_prev");
 		var btnNextEl = ctx.querySelector(".pagenation > .btn_next");		
 
-		var tabIndext = 0;
-		var tabContentsIndex = 0;
-		var MOVE_STEP = 1;
+		init();
 
+		//init
+		function init() {
+			btnNextEl.addEventListener("click", onNextBtnClick);
+			btnPrevEl.addEventListener("click", onPrevBtnClick);
+			onClickTabs();
+		}
 
+		//util
     function addClass(el, className) {
       el.classList.add(className);
 		}
@@ -39,54 +53,34 @@
 			el.classList.remove(className);
     }
 
-    // function removeClassAll(elems, className) {
-		// 	Array.prototype.forEach.call(elems, function (el) {
-		// 		removeClass(el, className)	
-		// 	});
-		// }
-
 		function removeClassAll(elems, classname) {
 			elems.forEach(function(el) {
 				removeClass(el, classname);
 			})
+		}		
+		
+		//movement
+		function isAbleToMove(targetIndex) {
+			if (!tabs[targetIndex]) return;
+			tabIndext = targetIndex;
+			tabContentsIndex = targetIndex;
 		}
 
-		// removeAll , addAll - (removeAll -> addClass) => removeAllAndAddOneClass
-    for (let i = 0; i < tabs.length; i++) {
-      tabs[i].addEventListener("click", function() {
-				removeClassAll(tabs, "on");
-				removeClassAll(tabContents, "visible");
-        addClass(tabs[i], "on");
-				addClass(tabContents[i], 'visible');
-				tabIndext = i;
-				tabContentsIndex = i;
-      });
+		function moveToNext(tabIndex) {
+			var nextIndex = tabIndext + MOVE_STEP;
+			isAbleToMove(nextIndex);
 		}
 
-		function isAbleToMoveNext(tabIndex) {
-			if (tabIndext < tabs.length -1) {
-				tabIndext += 1;
-				tabContentsIndex += 1;
-			} else {
-				tabIndext = 0;
-				tabContentsIndex = 0;
-			}
+		function moveToPrev(tabIndex) {
+			var prevIndex = tabIndext - MOVE_STEP;
+			isAbleToMove(prevIndex);
 		}
 
-		function isAbleToMovePrev(tabIndex) {
-			if (tabIndext <= 0) {
-				tabIndext = tabs.length -1;
-				tabContentsIndex = tabs.length -1;
-			} else {
-				tabIndext -= 1;
-				tabContentsIndex -= 1;
-			}
-		}
-
+		//events
 		function onNextBtnClick() {
 			removeClassAll(tabs, "on");
 			removeClassAll(tabContents, "visible");
-			isAbleToMoveNext(this.tabIndex);
+			moveToNext(this.tabIndex);
 			addClass(tabs[tabIndext], "on");
 			addClass(tabContents[tabContentsIndex], 'visible');
 		}
@@ -94,50 +88,23 @@
 		function onPrevBtnClick() {
 			removeClassAll(tabs, "on");
 			removeClassAll(tabContents, "visible");
-			isAbleToMovePrev(this.tabIndex);
+			moveToPrev(this.tabIndex);
 			addClass(tabs[tabIndext], "on");
 			addClass(tabContents[tabContentsIndex], 'visible');
 		}
 
-		btnNextEl.addEventListener("click", onNextBtnClick);
-		btnPrevEl.addEventListener("click", onPrevBtnClick);
-	
-    // btnNextEl.addEventListener("click", function() {
-
-			// 1. removeAll(elems, 'on')
-			// 2. isAbleToMoveNext(tabIndex) => true / false
-			// 3. increase(tabIndex, 1) / decrease(tabIndex, 1) => 1은 var MOVE_STEP = 1
-			// 4. addClass(elem, 'on')
-			// 5. addClass(elem, 'visible')
-			// 6. eventHandler로 만든다. onNextBtnClick
-			// 7. btnNextEl.addEventKistener('click', onNextBtnClickHandler)
-
-			// removeClassAll(tabs, "on");
-			// removeClassAll(tabContents, "visible");
-			// if (tabIndext >= tabs.length -1) {
-			// 	tabIndext = 0;
-			// 	tabContentsIndex = 0;
-			// } else {
-			// 	tabIndext += 1;
-			// 	tabContentsIndex += 1;
-			// }
-			// addClass(tabs[tabIndext], "on");
-			// addClass(tabContents[tabContentsIndex], 'visible');
-		// });
-
-		// btnPrevEl.addEventListener("click", function() {
-		// 	removeClassAll(tabs, "on");
-		// 	removeClassAll(tabContents, "visible");
-			// if (tabIndext <= 0) {
-			// 	tabIndext = tabs.length -1;
-			// 	tabContentsIndex = tabs.length -1;
-			// } else {
-			// 	tabIndext -= 1;
-			// 	tabContentsIndex -= 1;
-			// }
-			// addClass(tabs[tabIndext], "on");
-			// addClass(tabContents[tabContentsIndex], 'visible');
-		// });
+		function onClickTabs() {
+			tabs.forEach(function(tab, i) {
+				tab.addEventListener("click", function() {
+					removeClassAll(tabs, "on");
+					removeClassAll(tabContents, "visible");
+					addClass(tab, "on");
+					addClass(tabContents[i], 'visible');
+					tabIndext = i;
+					tabContentsIndex = i;
+				});
+			})	
+		}
 		
   };
 })();

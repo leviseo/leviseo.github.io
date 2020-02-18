63d4ba86dc7c5c350bd8b47d84a1f625541022fb
leviseo <seojuhee4@naver.com>
2020-02-19 03:32:18 +0900

    update practice 02_select 진행중

--- practice/02_select/select.js
+++ practice/02_select/select.js
@@ -1,13 +1,35 @@
 (function() {
 	'use strict';
 	window.onload = function() {
+		// breakfirst 일 때 input/ datalist 사용해보자
 		var ctx = document.querySelector('.container');
 		var selectQuestionEl = ctx.querySelector('#selectQuestion');
-		var optionQuestionEl = selectQuestionEl.getElementsByTagName('option');
+		var answerInpEl = ctx.querySelector('.tf_answer');
 
-		var selectValue = optionQuestionEl[selectQuestionEl.selectedIndex].value;
+		var selectedOption = selectQuestionEl.options[selectQuestionEl.selectedIndex].value;
 
-		console.log(selectValue);
+		console.log(selectedOption);
+
+		selectQuestionEl.addEventListener('change', function() {
+			selectedOption = event.target.value;
+			if (selectedOption === "password") {
+				answerInpEl.type = "password";
+				answerInpEl.value = "";
+			} else if (selectedOption === "currentDateTime") {
+				
+			}
+
+			
+			console.log(selectedOption);
+		})
+
+		function currentTime() {
+			var today = new Date();
+			var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+			console.log(today);
+		}
+		
+		// setInterval(currentTime, 1000);
 
 	}
 })();

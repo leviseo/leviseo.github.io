764290957f2eee08f2fc130831eafa910fcd5fe0
leviseo <seojuhee4@naver.com>
2020-02-21 22:08:42 +0900

    update practice03 VM

--- practice/03_vendingMachine/main.js
+++ practice/03_vendingMachine/main.js
@@ -1,16 +1,19 @@
 (function() {
 	"use strict";
   window.onload = function() {
-		/*
+		/* STEP1.
 		1. ~돈투입 (1000원)~ 나중에
 		2. 음료 선택
 		3. '1000원 - 음료 값' 거스름돈 에 적어줌
 				예산 0 원
 		?? itemCost 중 가장 낮은 거는 어캐 찾음??
 		*/
+		/* STEP2.
+		거스름돈이 100, 500, 1000, 5000, 10000 로 나와야함
+		*/
 
-		var budget = 0;
-		var change = 0;
+		var BUDGET = 0;
+		var CHANGE = 0;
 		var itemCost01 = 800;
 		var itemCost02 = 700;
 		var itemCount01 = 0;
@@ -27,13 +30,13 @@
 		var countItem02El = ctx.querySelector(".count_item02");	
 
 		btnInsertEl.addEventListener("click", function() {
-			budget += Number(tfCoinEl.value);
-			machineMoneyEl.innerHTML = budget;
+			BUDGET += Number(tfCoinEl.value);
+			machineMoneyEl.innerHTML = BUDGET;
 			tfCoinEl.value = 0;
-			if (budget >= itemCost02) {
+			if (BUDGET >= itemCost02) {
 				btnItem01El.disabled = false;
 				btnItem02El.disabled = false;
-			} else if (budget >= itemCost01) {
+			} else if (BUDGET >= itemCost01) {
 				btnItem02El.disabled = false;
 			}
 		})
@@ -41,11 +44,11 @@
 		btnItem01El.addEventListener("click", function() {
 			itemCount01 += 1;
 			countItem01El.innerHTML = itemCount01;
-			budget -= itemCost01;
-			machineMoneyEl.innerHTML = budget;
-			if (budget < itemCost02) {
-				change = budget;
-				changeMoneyEl.innerHTML = change;
+			BUDGET -= itemCost01;
+			machineMoneyEl.innerHTML = BUDGET;
+			if (BUDGET < itemCost02) {
+				CHANGE = BUDGET;
+				changeMoneyEl.innerHTML = CHANGE;
 				machineMoneyEl.innerHTML = 0;
 				btnItem01El.disabled = true;
 				btnItem02El.disabled = true;
@@ -55,11 +58,11 @@
 		btnItem02El.addEventListener("click", function() {
 			itemCount02 += 1;
 			countItem02El.innerHTML = itemCount02;
-			budget -= itemCost02;
-			machineMoneyEl.innerHTML = budget;
-			if (budget < itemCost02) {
-				change = budget;
-				changeMoneyEl.innerHTML = change;
+			BUDGET -= itemCost02;
+			machineMoneyEl.innerHTML = BUDGET;
+			if (BUDGET < itemCost02) {
+				CHANGE = BUDGET;
+				changeMoneyEl.innerHTML = CHANGE;
 				machineMoneyEl.innerHTML = 0;
 				btnItem01El.disabled = true;
 				btnItem02El.disabled = true;
